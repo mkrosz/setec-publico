fa834db3694ca6c342a71570b9030d24bb35be1a
mkrosz <mikewillyf@gmail.com>
2026-09-09 19:59:24 -0300

    fix: registrar integracao do tailwind no astro.config (estava faltando)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
+  integrations: [tailwind()],
   adapter: vercel({
     isr: false, // Desativa o Incremental Static Regeneration (força SSR em tempo real)
   }),
